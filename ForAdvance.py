1e7721df8075590be7465c823db6925127c97486
Apurva <apurvapimparkar@Apurvas-MacBook-Pro.local>
2017-11-13 13:15:57 -0800

    Python program exercise-2

---
+++ ForAdvance.py
@@ -0,0 +1,54 @@
+#Excercise - 2
+
+#Program-1
+for i in range(101):
+    print('Apurva')
+
+#Program-2
+for i in range(50):
+    print('Apurva', end=' ')
+
+#Program-3
+for i in range (1,51):
+    print(i, 'Apurva')
+    
+#Program-4
+for i in range(1,21):
+    print(i,'---', i**2)
+    
+#Program-5
+for i in range(8,91,3):
+    print(i, end=' ')
+
+#Program-6
+for i in range(100,1,-2):
+    print(i, end=' ')
+
+#Program-7
+name=input('Enter your name: ')
+num=eval(input('How many times you want to print your name? '))
+for i in range(num):
+    print(name)
+
+#Program-8
+num=eval(input('Enter triangle height: '))
+for i in range(num):
+    print('*' * (num-i))
+    
+#Program-9
+num=eval(input('Enter triangle height: '))
+for i in range(num):
+    print(' '* (num-i), '*'*(i*2-1))
+
+    
+
+
+    
+
+    
+    
+
+          
+
+    
+    
